ef9d626fe645f6daba89fb84f7c64077f5fbe5f4
Malte <mbo@demodern.de>
2020-04-28 16:09:17 +1200

    Clean up Home mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,62 +1,29 @@
-import React, { FC, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import ApolloClient, { gql } from 'apollo-boost';
-import { Provider, useDispatch } from 'react-redux';
+import React, { FC } from 'react';
+import ApolloClient from 'apollo-boost';
+import { Provider } from 'react-redux';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { ApolloProvider } from '@apollo/react-hooks';
 import configureStore from './configureStore';
 import { Window } from './types';
-import { setTheme } from './store/layout';
+import HomeScreen from './src/screens/Home';
 
 declare let window: Window & typeof globalThis;
 
 const store = configureStore(window.INITIAL_REDUX_STATE);
 
-const client = new ApolloClient({
-  uri: 'http://localhost:8080/',
+export const client = new ApolloClient({
+  uri: process.env.GRAPHQL_URI,
 });
 
-client
-  .query({
-    query: gql`
-      {
-        users {
-          name
-          email
-        }
-      }
-    `,
-  })
-  .then(() => null);
-
-const Home: FC = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(setTheme('dark'));
-  }, []);
-
-  return null;
-};
-
 const App: FC = () => (
-  <ApolloProvider client={client}>
-    <Provider store={store}>
-      <View style={styles.container}>
-        <Home />
-        <Text>Open up App.tsx to start working on your app!</Text>
-      </View>
-    </Provider>
-  </ApolloProvider>
+  <SafeAreaProvider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <HomeScreen />
+      </Provider>
+    </ApolloProvider>
+  </SafeAreaProvider>
 );
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
