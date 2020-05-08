2dfe796bc567716a56b92240f92ae30f465b5bfc
Malte <mbo@demodern.de>
2020-05-08 14:16:37 +1200

    Add login slider

--- mobile/src/screens/components/LoginForm.tsx
+++ mobile/src/screens/components/LoginForm.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from 'react';
+import React, { FC, useState, ReactNode } from 'react';
 import { Alert, TextInput } from 'react-native';
 import { useForm, Controller } from 'react-hook-form';
+import SideSwipe from 'react-native-sideswipe';
 
 import {
   Panel,
@@ -16,35 +17,22 @@ type FormData = {
   password: string;
 };
 
-const LoginForm: FC = () => {
+const SmallTextConfig: TextProps = {
+  mode: 'day',
+  appearance: 'heavy',
+  type: 'small',
+  bold: true,
+};
+
+const Login: FC = () => {
   const { control, handleSubmit, errors } = useForm<FormData>();
 
   const onSubmit = (data: FormData): void => Alert.alert('Form Data', JSON.stringify(data));
-
-  const SmallTextConfig: TextProps = {
-    mode: 'day',
-    appearance: 'heavy',
-    type: 'small',
-    bold: true,
-  };
-
   return (
-    <Panel marginHorizontal>
-      <Panel row marginBottom={1.5}>
-        <Button mode="day" appearance="strong" type="normal" marginRight={0.5} style={{ flex: 1 }}>
-          Login
-        </Button>
-        <Button
-          mode="day"
-          appearance="strong"
-          type="normal"
-          outline
-          marginLeft={0.5}
-          style={{ flex: 1 }}
-        >
-          Register
-        </Button>
-      </Panel>
+    <Panel
+      marginHorizontal
+      style={{ width: AssetStyles.measure.window.width - AssetStyles.measure.space * 2 }}
+    >
       {errors.email && <Text {...SmallTextConfig}>This is required.</Text>}
       <Controller
         as={TextInput}
@@ -83,6 +71,47 @@ const LoginForm: FC = () => {
         onPress={handleSubmit(onSubmit)}
       />
     </Panel>
+  );
+};
+
+const LoginForm: FC = () => {
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+  return (
+    <>
+      <Panel row marginHorizontal marginBottom={1.5}>
+        <Button
+          mode="day"
+          appearance="strong"
+          outline={currentIndex !== 0}
+          type="normal"
+          marginRight={0.5}
+          style={{ flex: 1 }}
+          onPress={(): void => setCurrentIndex(0)}
+        >
+          Login
+        </Button>
+        <Button
+          mode="day"
+          appearance="strong"
+          type="normal"
+          outline={currentIndex !== 1}
+          marginLeft={0.5}
+          style={{ flex: 1 }}
+          onPress={(): void => setCurrentIndex(1)}
+        >
+          Register
+        </Button>
+      </Panel>
+      <SideSwipe
+        index={currentIndex}
+        itemWidth={AssetStyles.measure.window.width}
+        style={{ width: AssetStyles.measure.window.width }}
+        data={[{ title: 'test' }, { title: 'test2' }]}
+        onIndexChange={(index): void => setCurrentIndex(index)}
+        renderItem={(): ReactNode => <Login />}
+      />
+    </>
   );
 };
 
