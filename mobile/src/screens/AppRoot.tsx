2634db87cbe0096fe8fab1a8dbc29f73384a6865
Malte <mbo@demodern.de>
2020-05-10 13:44:13 +1200

    Add auth navigation switch

--- mobile/src/screens/AppRoot.tsx
+++ mobile/src/screens/AppRoot.tsx
@@ -2,8 +2,10 @@ import React, { FC } from 'react';
 import { StatusBar } from 'react-native';
 import { createStackNavigator, StackNavigationProp } from '@react-navigation/stack';
 import { RouteProp, CompositeNavigationProp } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
 import HomeRoot, { ParentParamList } from './HomeRoot';
 import OnboardingRoot from './OnboardingRoot';
+import { userSelector } from '../store/user';
 
 export type AppRootParamList = {
   HomeRoot: undefined;
@@ -27,12 +29,16 @@ type NavigationProps = {
 const Stack = createStackNavigator<AppRootParamList>();
 
 const AppRoot: FC<NavigationProps> = () => {
+  const { name } = useSelector(userSelector);
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <Stack.Navigator headerMode="none" initialRouteName="OnboardingRoot">
-        <Stack.Screen name="HomeRoot" component={HomeRoot} />
-        <Stack.Screen name="OnboardingRoot" component={OnboardingRoot} />
+      <Stack.Navigator headerMode="none">
+        {name ? (
+          <Stack.Screen name="HomeRoot" component={HomeRoot} />
+        ) : (
+          <Stack.Screen name="OnboardingRoot" component={OnboardingRoot} />
+        )}
       </Stack.Navigator>
     </>
   );
