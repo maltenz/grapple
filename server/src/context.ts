f5bd22b9edc765dc48644fa8fbc46d6a2e151d61
Malte <mbo@demodern.de>
2020-05-14 09:23:34 +1200

    Type check context

--- server/src/context.ts
+++ server/src/context.ts
@@ -1,17 +1,25 @@
+import mongoose from 'mongoose';
 import { getConnection } from './database/Provider';
 import authenticate from './helper/authenticate';
 
 /**
  * @description holds context for Apollo Server
+ * @param dbConn
+ * @param loggedIn
+ * @returns {Context}
  */
 
-export const context = async (req): Promise<any> => {
+interface Context {
+  dbConn: mongoose.Connection;
+  loggedIn: boolean;
+}
+
+export const context = async (req: mongoose.Connection): Promise<Context> => {
   const dbConn = await getConnection();
   const loggedIn = await authenticate(dbConn, req);
 
   return {
     dbConn,
     loggedIn,
-    token: '',
   };
 };
