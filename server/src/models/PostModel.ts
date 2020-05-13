b8a26a66dc216c2d9eab16e4635638e7caec94f4
Malte <mbo@demodern.de>
2020-05-14 09:50:37 +1200

    Add post model

---
+++ server/src/models/PostModel.ts
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose';
+
+/**
+ * @description holds post model
+ */
+
+/**
+ * Post interface
+ */
+export interface IPost extends mongoose.Document {
+  id: string;
+  title: string;
+  content: string;
+  image: string;
+  transform: () => IPost;
+}
+
+/**
+ * post schema
+ */
+const schema: mongoose.SchemaDefinition = {
+  title: { type: mongoose.SchemaTypes.String, required: true },
+  content: { type: mongoose.SchemaTypes.String, required: true },
+  image: { type: mongoose.SchemaTypes.String, required: true },
+};
+
+// post collection name
+const collectionName = 'post';
+
+const postSchema: mongoose.Schema = new mongoose.Schema(schema);
+
+/**
+ * transforms post object
+ * changes _id to id
+ */
+postSchema.methods.transform = function (): any {
+  const obj = this.toObject();
+
+  const id = obj._id;
+  delete obj._id;
+  obj.id = id;
+
+  return obj;
+};
+
+/**
+ * creates post model
+ * @param conn database connection
+ * @returns post model
+ */
+const PostModel = (conn: mongoose.Connection): mongoose.Model<IPost> =>
+  conn.model(collectionName, postSchema);
+
+export default PostModel;
