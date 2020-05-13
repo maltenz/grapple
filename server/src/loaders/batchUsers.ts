f662b1e1638bcc7ea23ff5602fc7774cdd8f915d
Malte <mbo@demodern.de>
2020-05-14 09:56:11 +1200

    Remove batched users

--- server/src/loaders/batchUsers.ts
+++
@@ -1,9 +0,0 @@
-import UserModel from '../models/UserModel';
-
-const batchUsers = async ({ dbConn }, userIds) => {
-  console.log('keys====', userIds);
-  const users = await UserModel(dbConn).find({ _id: { $in: userIds } });
-  return userIds.map((userId) => users.find((user) => user.id === userId));
-};
-
-export { batchUsers };
