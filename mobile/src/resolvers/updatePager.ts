0ffa25f7733d5a2c9573853e14df60bf1787dca9
Malte <mbo@demodern.de>
2020-05-22 08:31:47 +1200

    Remove old page resolver

--- mobile/src/resolvers/updatePager.ts
+++ mobile/src/resolvers/updatePager.ts
@@ -1,19 +1,18 @@
 /* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any */
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import ApolloClient from 'apollo-client';
+
 import {
-  PagerDataFragment,
   GetPagerQuery,
   GetPagerDocument,
   UpdatePagerMutationVariables,
 } from '../generated/graphql';
 
 export default function updatePager(
-  root: any,
+  root: {},
   variables: UpdatePagerMutationVariables,
-  context: { cache: InMemoryCache; getCacheKey: any; client: ApolloClient<any> },
+  context: { cache: InMemoryCache },
   info: any
-): any {
+): void {
   const { cache } = context;
 
   const pager = getPager(cache);
@@ -29,7 +28,6 @@ export default function updatePager(
       },
     });
   }
-  return null;
 }
 
 function getPager(cache: InMemoryCache): any {
