c1d9526720709bbe59980dd30e94f9dc348ddc7e
Malte <mbo@demodern.de>
2020-06-17 10:27:15 +1200

    Add heroku port

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,6 +9,6 @@ import apolloServer from './graphql';
 
 const port: string = process.env.PORT as string;
 
-apolloServer.listen(port).then(({ url }) => {
+apolloServer.listen(port || 8080).then(({ url }) => {
   console.log(`Apollo Server is running on ${url} `);
 });
