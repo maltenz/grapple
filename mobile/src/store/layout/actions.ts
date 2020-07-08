7f63f5dc38c8b080109b355285025765c013748b
Malte <mbo@demodern.de>
2020-07-09 10:45:54 +1200

    Clean up root config

--- mobile/src/store/layout/actions.ts
+++ mobile/src/store/layout/actions.ts
@@ -1,8 +1,7 @@
 import { action, Action } from 'typesafe-actions';
 
-import { Modal } from 'react-native';
 import { LayoutActionTypes } from './types';
-import { Pager } from '../../generated/graphql';
+import { Pager, Modal } from '../../generated/graphql';
 
 export const updatePager = (pager: Pager): Action => action(LayoutActionTypes.UPDATE_PAGER, pager);
 export const updateModal = (modal: Modal): Action => action(LayoutActionTypes.UPDATE_MODAL, modal);
