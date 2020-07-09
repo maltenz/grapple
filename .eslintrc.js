77fcac209965242b2e656aeb5d741b2f9711e27d
Malte <mbo@demodern.de>
2020-07-09 13:57:58 +1200

    Ignore explicit-module-boundary-types

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
-   "parser": "@typescript-eslint/parser",
-   "extends": [
-     "eslint:recommended",
-     "plugin:@typescript-eslint/eslint-recommended",
-     "plugin:@typescript-eslint/recommended",
-     "plugin:prettier/recommended",
-   ],
-   "rules": {
-    "@typescript-eslint/ban-ts-ignore": "off",
-    "prettier/prettier": [
-      "error",
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    'prettier/prettier': [
+      'error',
       {
-        "trailingComma": "es5",
-        "singleQuote": true,
-        "printWidth": 100,
-        "semi": true
-      }
+        trailingComma: 'es5',
+        singleQuote: true,
+        printWidth: 100,
+        semi: true,
+      },
     ],
-   },
-   "env": {
-     "browser": true,
-     "node": true
-   },
-}
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+};
--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
-  "parser": "@typescript-eslint/parser",
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "prettier/flowtype",
-    "prettier/standard"
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/flowtype',
+    'prettier/standard',
   ],
-  "rules": {
-    "eqeqeq":["error", "always"],
-   },
-  "env": {
-    "browser": true,
-    "node": true
+  rules: {
+    eqeqeq: ['error', 'always'],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
-}
+  env: {
+    browser: true,
+    node: true,
+  },
+};
