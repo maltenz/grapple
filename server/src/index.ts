94737a769ab8548032600df6036cb8e021db153f
Malte <mbo@demodern.de>
2020-07-09 16:33:31 +1200

    Fix a whole bunch of TS

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,4 +10,7 @@ dotenv.config();
 
 const port: string = process.env.PORT as string;
 
-apolloServer.listen(port || 8080).then(({ url }) => `Apollo Server is running on ${url} `);
+apolloServer.listen(port || 8080).then(({ url }) => {
+  // eslint-disable-next-line no-console
+  console.log(`Apollo Server is running on ${url} `);
+});
