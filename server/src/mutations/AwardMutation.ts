dc7754e1ab15ea4e4d74537f21eee83a1b35619a
Malte <mbo@demodern.de>
2020-07-09 17:42:48 +1200

    Fix all linting errors

--- server/src/mutations/AwardMutation.ts
+++ server/src/mutations/AwardMutation.ts
@@ -2,10 +2,16 @@
 import { createAward } from '../controllers/AwardController';
 import { Award } from '../models/AwardModel';
 import { Context } from '../context';
+import { AwardInput } from '../generated/graphql';
 
 export const AwardMutation = {
   createAward: {
-    resolve: async (parent, args, context: Context, info): Promise<Award> => {
+    resolve: async (
+      parent,
+      args: { input: AwardInput },
+      context: Context,
+      info
+    ): Promise<Award> => {
       return createAward(context, args);
     },
   },
