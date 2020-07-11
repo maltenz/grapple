506deec0c39331c8e42ef08350cae29aabd75be5
Malte <mbo@demodern.de>
2020-07-11 20:46:33 +1200

    Change port to 3000

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ dotenv.config();
 
 import apolloServer from './graphql';
 
-apolloServer.listen(process.env.PORT || 8080).then(({ url }) => {
+apolloServer.listen(process.env.PORT || 3000).then(({ url }) => {
   // eslint-disable-next-line no-console
   console.log(`Apollo Server is running on ${url} `);
 });
